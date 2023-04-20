b6a36d6513bb6ca96eee7841c02a726883a7bd53
Piotr Tomczak <piotr.t@niceguys.info>
2023-04-20 15:58:10 +0200

    Sidebar half done

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
-import { HTMLProps } from "react";
+import { HTMLProps, ReactNode, ReactSVGElement } from "react";
 
 type ButtonProps = {
   backgroundColor: string;
   textColor: string;
-  hoverColor: string;
+  hoverColor?: string;
   content: string;
   href?: string;
+  icon?: ReactNode;
 } & Omit<HTMLProps<HTMLButtonElement>, "type">;
 
 export const Button = ({
@@ -15,16 +16,18 @@ export const Button = ({
   hoverColor,
   content,
   href,
+  icon,
   ...props
 }: ButtonProps) => {
   const realTextColor = textColor.startsWith("#")
     ? `text-[${textColor}]`
     : `text-${textColor}`;
   const buttonContent = href ? <Link href={href}>{content}</Link> : content;
-  const className = `flex h-10 justify-center font-bold items-center rounded-xl px-2 ${realTextColor} hover:bg-[#9BBFD4] bg-[${backgroundColor}] text-md hover:cursor-pointer text-center disabled:bg-[#9BBFD4] hover:shadow-lg`;
+  const className = `flex gap-2 h-10 justify-center font-bold items-center rounded-xl px-2 ${realTextColor} hover:bg-[${hoverColor}] bg-[${backgroundColor}] text-md hover:cursor-pointer text-center disabled:bg-[#9BBFD4] hover:shadow-lg`;
 
   return (
     <button className={className} {...props}>
+      {icon && icon}
       {buttonContent}
     </button>
   );
--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Button } from "../Button";
+import humans from "../../assets/humans.svg";
+import arrow from "../../assets/arrow.svg";
+import Image from "next/image";
 
 export const Sidebar = () => {
   const containerStyle = {
@@ -12,9 +15,19 @@ export const Sidebar = () => {
       style={containerStyle}
       className="flex flex-col h-full p-6 w-2/12 items-stretch justify-between"
     >
-      <div className="flex flex-col h-[80%] justify-start items-baseline">
-        <Button textColor="" content="" backgroundColor="" hoverColor="" />
-        <Button textColor="" content="" backgroundColor="" hoverColor="" />
+      <div className="flex flex-col h-[80%] gap-2 justify-start items-baseline">
+        <Button
+          textColor="#699FBF"
+          content="Moje pliki"
+          backgroundColor="white"
+          icon={<Image src={arrow} width={20} height={20} alt="arrow" />}
+        />
+        <Button
+          textColor="#699FBF"
+          content="Udostępnione dla mnie"
+          backgroundColor="white"
+          icon={<Image src={humans} width={20} height={20} alt="arrow" />}
+        />
       </div>
       <div className="flex flex-col h-[80%] justify-end items-center">xsss</div>
     </div>
